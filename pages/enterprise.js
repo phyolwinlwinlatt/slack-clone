bf3edbd39f931368ce9b9b48a42571e1c0e301b9
phyolwinlwinlatt <phyolwinlwinlatt@gmail.com>
2022-01-17 15:35:11 +0630

    Initial Commit

---
+++ pages/enterprise.js
@@ -0,0 +1,3 @@
+export default function Enterprise() {
+  return <div> Enterprise</div>;
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from "../containers/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>Home layout</div>
+    </Layout>
+  );
+}
